bf6a1c7f4a7afc9f8534a88e28f39968446f9503
tellaweb <templeoweb2425@gmail.com>
2025-10-07 11:49:46 +0200

    Delete js directory

--- js/hamburguesa.js
+++
@@ -1,5 +0,0 @@
-function menu() {
-    const container = document.querySelector('.ocultarMenu');
-    // Se revisa el estado actual y se alterna entre 'block' y 'none'
-    container.style.display = (container.style.display === 'block' ? 'none' : 'block');
-}
